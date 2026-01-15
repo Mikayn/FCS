2c38c5d6c7eff520f3733ce942d4ab3d59ab94d3
Mikayn <ghimirearyan21@gmail.com>
2026-01-15 09:32:16 +0000

    Updated QuotedPrintable.py

--- Task 1/QuotedPrintable.py
+++ Task 1/QuotedPrintable.py
@@ -1,7 +1,16 @@
-from quopri import encodestring, decodestring
+# Importing necessary functions from Quoted Printable module.
+from quopri import encodestring, decodestring 
 
-text = input("Enter a word from 8-bit ASCII: ")
+# Take user input
+text = input("\nEnter a word from 8-bit ASCII: ")
+print("") # For spacing to see the output better.
 
+# The "encodestring" function takes data in bytes, not a string. 
 dataInBytes = text.encode("UTF-8")
+encodedBytes = (encodestring(dataInBytes))
+print(encodedBytes, "\n")
 
-print(encodestring(dataInBytes))
+# The "decodedstring" function gives the data in bytes.
+decodedBytes = decodestring(encodedBytes)
+decodedString = decodedBytes.decode("utf-8")
+print(decodedString)
